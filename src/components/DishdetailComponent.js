4f0c0bbb311e3f4efcc86588977cca348a0a6b7e
Hans Hokka <hans.hokka@gmail.com>
2020-03-15 10:31:21 +0200

    Single Page Applications part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Link } from "react-router-dom";
 
 class Dishdetail extends Component {
   renderDish() {
